a5845be8aefb0b67e43611a6efcbfa62e6b32a26
goziecodes <ezechukwuchigozie@gmail.com>
2020-06-04 16:28:39 +0100

    fixed firebase auth

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -4,12 +4,12 @@ import 'firebase/auth';
 
 const config = {
   apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
+  authDomain: 'clothing-db-bbc3d.firebaseapp.com',
+  databaseURL: 'https://clothing-db-bbc3d.firebaseio.com',
+  projectId: 'clothing-db-bbc3d',
+  storageBucket: 'clothing-db-bbc3d.appspot.com',
   messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  appId: '1:28166265523:web:e509c68e01aab04091eec6'
 };
 
 firebase.initializeApp(config);
