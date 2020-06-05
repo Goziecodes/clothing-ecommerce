8276f0bd9bc8301f94e3e9fdc2d98cec55a1a61c
goziecodes <ezechukwuchigozie@gmail.com>
2020-06-05 01:47:07 +0100

    last fix

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -46,7 +46,7 @@ const CheckoutPage = ({ cartItems, total }) => (
       <br />
       4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
     </WarningContainer>
-    <StripeCheckoutButton price={total} />
+    <StripeCheckoutButton price={total} /> 
   </CheckoutPageContainer>
 );
 
